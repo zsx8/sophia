732f255de017bfd050f096ff9870fd49bc95b80b
zsx8 <zsakkaya4@gmail.com>
2026-08-23 22:14:39 +0200

    Initial Commit

---
+++ sophia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sophia
+//
+//  Created by zs on 23.08.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sophia/sophiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  sophiaApp.swift
+//  sophia
+//
+//  Created by zs on 23.08.26.
+//
+
+import SwiftUI
+
+@main
+struct sophiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
